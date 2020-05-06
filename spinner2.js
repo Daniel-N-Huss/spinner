a0599f249fa0ec522f2fad7552ba63ff766e4ebd
Daniel-N-Huss <daniel.n.huss@gmail.com>
2020-05-06 00:02:24 +0000

    refactor animator to be inline function, and accomodate the nature of passing references vs values into async

--- spinner2.js
+++ spinner2.js
@@ -1,19 +1,31 @@
-const spinnerFunc = function(frame, delay) {
-  setTimeout(() => {
-    process.stdout.write(frame);
-  }, delay);
-};
-
 let frames = ['\r|    ', '\r/    ', '\r-    ',  '\r\\    '];
 
 const animator = function(numOfLoops) {
   let count = 0;
-  const delay = 200;
   while (count < numOfLoops * 4 + 1) {
-    spinnerFunc(frames[0], delay * count);
+    const frame = frames[0];
+    setTimeout(() => {
+      process.stdout.write(frame);
+    }, 200 * count);
     count++;
     frames.push(frames.shift());
   }
 };
 
 animator(5);
+
+//results of Timeoutcs
+
+// const animator = function(numOfLoops) {
+  // let count = 0;
+  // const delay = 200;
+  // while (count < numOfLoops * 4 + 1) {
+    
+    // (function () {
+      // setTimeout(() =>
+        // process.stdout.write(frames[0]), delay * count);
+      // count++;
+      // frames.push(frames.shift());
+    // });
+  // }
+// }
